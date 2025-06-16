2cd0f6bfc2479479676697aff60332984364fc94
ddavedd <david.j.smits@gmail.com>
2025-06-16 07:32:56 -0500

    Update get_status_8.sh

--- ghcontrol/get_status_8.sh
+++ ghcontrol/get_status_8.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-/home/USERNAME/ghcontrol/log_event.sh "/usr/local/bin/8relind 0 read: $(/usr/local/bin//usr/local/bin/8relind 0 read)"
+/home/USERNAME/ghcontrol/log_event.sh "/usr/local/bin/8relind 0 read: $(/usr/local/bin/8relind 0 read)"
