dca7c52dbe3f27c8f942329e89c540b8b50dd652
ddavedd <david.j.smits@gmail.com>
2025-06-16 07:32:31 -0500

    Update get_status_3.sh

--- ghcontrol/get_status_3.sh
+++ ghcontrol/get_status_3.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-/home/USERNAME/ghcontrol/log_event.sh "/usr/local/bin/3relind 0 read: $(/usr/local/bin//usr/local/bin/3relind 0 read)"
+/home/USERNAME/ghcontrol/log_event.sh "/usr/local/bin/3relind 0 read: $(/usr/local/bin/3relind 0 read)"
