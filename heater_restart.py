d0a2c6ca600d0f02212720b6b7288e0268366851
David Smits <david.j.smits@gmail.com>
2026-02-20 22:21:41 -0600

    Heater restart, still needs to be called in crontab upon reboot

--- heater_restart.py
+++ heater_restart.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-with open("heater_enable_on_restart.txt") as f:
-    enable = int(f.read())
-    if enable == 1:
-        
+import os
+from os.path import isfile, join
+username = os.getlogin()
+heater_on_scripts = []
+with open("/home/%s/ghcontrol/heater_enable_on_restart.txt" % username) as f:
+   enable = int(f.read())
+   if enable == 1:
+      scripts_directory = "/home/%s/ghcontrol/scripts" % username
+      files = [f for f in os.listdir(scripts_directory) if isfile(join(scripts_directory,f))]
+      for f in files:
+         if f.startswith("Heater") and f.endswith("Enable_On.sh"):
+            heater_on_scripts.append(f)
+for f in heater_on_scripts:
+   print("Running script " + f)
+   os.system("/home/%s/ghcontrol/scripts/%s" % (username, f))
