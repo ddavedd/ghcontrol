96daf88b7b5ba7462ed74d143e6a0a9a02815be5
David Smits <david.j.smits@gmail.com>
2026-02-23 22:28:54 -0600

    updated

--- templates/get_status.template.py
+++ templates/get_status.template.py
@@ -1,11 +1,14 @@
 #! /usr/bin/python3
 import os
+import subprocess
 username = os.getlogin()
 for number in [3,8]:
-   #output = os.system("/usr/bin/%irelind -list" % number)
-   output = "2 board(s) detected\nId: 1 0\n"
+   output = subprocess.check_output(["/usr/local/bin/%irelind"%number,"-list"], text=True)
+   #output = "2 board(s) detected\nId: 1 0\n"
    boards = output.split("\n")[1].split(" ")[1:]
    text = ""
    for b in boards:
-      text += "/home/%s/ghcontrol/log_event.sh \"/usr/local/bin/%irelind %s read: $(/usr/local/bin/%irelind %s read)\"\n" % (username, number, b, number, b)
-   print(text)
+      text += "/home/%s/ghcontrol/scripts/log_event.sh \"/usr/local/bin/%irelind %s read: $(/usr/local/bin/%irelind %s read)\"\n" % (username, number, b, number, b)
+   with open("/home/%s/ghcontrol/scripts/get_status_%i.sh" % (username, number), "w", encoding="utf-8") as f:
+      f.write(text)
+
