23858c3c5c84f102c416adaa494b0fc7bb84bc21
David Smits <david.j.smits@gmail.com>
2026-03-05 18:39:01 -0600

    Read password from file in script

--- report_upload/upload_to_site.sh
+++ report_upload/upload_to_site.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+FARM_PW=$(cat /home/$USER/ghcontrol/report_upload/.ftp_pass)
 cd /home/${USER}/ghcontrol/report_upload
 #a=(*)
 #cp ${a[@]: -1} westchestertoday.html
 ftp -nvi ftp.thefarmwestmont.com << END_SCRIPT
-user thefarmwestmontcom {cat /home/$USER/ghcontrol/report_upload/.ftp_pass}
+user thefarmwestmontcom $FARM_PW
 cd gh
 mkdir 2026
 cd 2026
