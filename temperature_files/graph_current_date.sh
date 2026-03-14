8e930bf887e14c52f5154346ef3807c3b310fcdc
David Smits <david.j.smits@gmail.com>
2026-03-14 15:16:13 -0500

    temperature files change struct

--- temperature_files/graph_current_date.sh
+++ temperature_files/graph_current_date.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 date_current=$(date +"%Y-%m-%d")
-/usr/bin/python3 /home/$USER/ghcontrol/temperature_files/temp_plot.py $date_current.tempf
+/usr/bin/python3 /home/$USER/ghcontrol/temperature_files/temp_plot.py /home/$USER/ghcontrol/temperature_files/tempf/$date_current.tempf
