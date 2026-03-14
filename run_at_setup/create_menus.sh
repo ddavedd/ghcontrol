8e03ace128b80e5889a5d0a9180458d8edda30ed
David Smits <david.j.smits@gmail.com>
2026-03-14 09:55:52 -0500

    Menu creation script to run after setting up the connections

--- run_at_setup/create_menus.sh
+++ run_at_setup/create_menus.sh
@@ -1,2 +1,3 @@
+/home/${USER}/ghcontrol/templates/clean_scripts_directory_desktop_entries.sh
 /usr/bin/python3 /home/${USER}/ghcontrol/templates/create_scripts_from_connections.py
 /home/${USER}/ghcontrol/templates/make_scripts_executable.sh
