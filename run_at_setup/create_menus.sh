45a20f8dedddb181d6a7dcd083cf41b0a2b68955
David Smits <david.j.smits@gmail.com>
2026-03-20 19:37:31 -0500

    Scripts need to make executable, and log and status need to be in create menus

--- run_at_setup/create_menus.sh
+++ run_at_setup/create_menus.sh
@@ -1,3 +1,5 @@
 /home/${USER}/ghcontrol/templates/clean_scripts_directory_desktop_entries.sh
 /usr/bin/python3 /home/${USER}/ghcontrol/templates/create_scripts_from_connections.py
+/usr/bin/python3 /home/${USER}/ghcontrol/templates/get_status.template.py
+sed "s/USERNAME/${USER}/g" /home/${USER}/ghcontrol/templates/log_event.sh.template > /home/${USER}/ghcontrol/scripts/log_event.sh
 /home/${USER}/ghcontrol/templates/make_scripts_executable.sh
