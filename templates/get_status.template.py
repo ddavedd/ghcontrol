7d8b656fdcd19707e5971710d98618d91291f2b5
David Smits <david.j.smits@gmail.com>
2026-03-21 08:47:58 -0500

    Initial step to read what is on in get status

--- templates/get_status.template.py
+++ templates/get_status.template.py
@@ -2,11 +2,15 @@
 import os
 import subprocess
 username = os.getlogin()
+
 for number in [3,8]:
    output = subprocess.check_output(["/usr/local/bin/%irelind"%number,"-list"], text=True)
    #output = "2 board(s) detected\nId: 1 0\n"
    boards = output.split("\n")[1].split(" ")[1:]
-   text = ""
+   text = "sensor_map = []\n"
+   text += "with open(\"/home/%s/ghcontrol/%s_comp_read.map\" % (username,username)) as mapfile:\n"
+   text += "\tfor line in mapfile.readlines():\n"
+   text += "\t\tsensor_map.append(line.strip().split(\",\"))\n"
    for b in boards:
       text += "/home/%s/ghcontrol/scripts/log_event.sh \"/usr/local/bin/%irelind %s read: $(/usr/local/bin/%irelind %s read)\"\n" % (username, number, b, number, b)
    with open("/home/%s/ghcontrol/scripts/get_status_%i.sh" % (username, number), "w", encoding="utf-8") as f:
