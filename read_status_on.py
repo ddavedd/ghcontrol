d589d01a4c17ef273e6c70cd2ec91976018aaf3f
ddavedd <david.j.smits@gmail.com>
2026-04-03 15:21:31 -0500

    Create read_status_on.py only working for hh1

---
+++ read_status_on.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+with open("hh1_comp_read.map") as f:
+	connections = f.readlines()
+	
+if len(connections)>0:
+	total_string = ""
+	for c in connections:
+		x = c.strip().split(',')
+		if int(subprocess.check_output("%irelind %i read %i" % (int(x[0]),int(x[1]),int(x[2])), shell=True)) == 1:
+			total_string += x[3] + " | "
+	print(total_string)
