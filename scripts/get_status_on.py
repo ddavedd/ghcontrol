03e20ec128944daa2589d82dcb592009bad6ec12
ddavedd <david.j.smits@gmail.com>
2026-04-06 10:06:27 -0500

    Update get_status_on.py

--- scripts/get_status_on.py
+++ scripts/get_status_on.py
@@ -1,16 +1,14 @@
 import os
 import subprocess
 username = os.getlogin()
-with open("/home/%s/ghcontrol/%s_comp_read.map" % (username, username)) as f:
-   lines = f.readlines()
 
-on_names = "| ON: "
-for l in lines:
-   s = l.strip().split(",")
-   command = "/usr/local/bin/%srelind" % s[0]
-   x = subprocess.run([command, s[1], "read", s[2]],capture_output=True)
-   if int(x.stdout) == 1:
-      on_names += (s[3] + " ")
-on_names += " |\n"
-print(on_names)
-
+with open("/home/%s/ghcontrol/%s_comp_read.map" % (username,username)) as f:
+	connections = f.readlines()
+	
+if len(connections)>0:
+	total_string = ""
+	for c in connections:
+		x = c.strip().split(',')
+		if int(subprocess.check_output("%irelind %i read %i" % (int(x[0]),int(x[1]),int(x[2])), shell=True)) == 1:
+			total_string += x[3] + " " + x[4] + " | "
+	print(total_string)
