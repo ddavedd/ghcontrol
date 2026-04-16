0314bd393246f3ba0500ddd3c16c20af6a575008
David Smits <david.j.smits@gmail.com>
2026-04-16 07:03:32 -0500

    Assemble cron file from parts and use install cron shell file to install

---
+++ cron/assemble_cron_file.sh
@@ -0,0 +1,2 @@
+cd /home/$USER/ghcontrol/cron
+cat cron_start.cron.template cron_reboot.cron.template cron_status.cron.template cron_graphs_and_report.cron.template cron_haf_schedule.cron.template > assembled.cron
