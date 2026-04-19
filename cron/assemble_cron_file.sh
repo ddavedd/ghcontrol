69ee309b100385c9920c52647da2aef272e79cfa
ddavedd <david.j.smits@gmail.com>
2026-04-19 12:10:39 -0500

    Update assemble_cron_file.sh

--- cron/assemble_cron_file.sh
+++ cron/assemble_cron_file.sh
@@ -2,5 +2,5 @@ cd /home/$USER/ghcontrol/cron
 cat cron_start.cron.template cron_reboot.cron.template cron_status.cron.template cron_graphs_and_report.cron.template cron_haf_schedule.cron.template > assembled.cron
 CRON_LOG=/home/$USER/ghcontrol/logs/$(date +"%F").cron.log
 USERNAME=$(id -un)
-sed s/USERNAME/$USERNAME/g assembled.cron > username_replaced.cron
-sed s/CRON_LOG/$CRON_LOG/g username_replaced.cron > gh.cron
+sed 's/USERNAME/$USERNAME/g' assembled.cron > username_replaced.cron
+sed 's/CRON_LOG/$CRON_LOG/g' username_replaced.cron > gh.cron
