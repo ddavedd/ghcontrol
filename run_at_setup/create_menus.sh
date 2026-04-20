063ff3cada2802aa9f04bf2cbc9f3e26928a903c
David Smits <david.j.smits@gmail.com>
2026-04-20 10:50:55 -0500

    Restructure setup scripts and templates

--- run_at_setup/create_menus.sh
+++ run_at_setup/create_menus.sh
@@ -1,5 +1,5 @@
-/home/${USER}/ghcontrol/templates/clean_scripts_directory_desktop_entries.sh
-/usr/bin/python3 /home/${USER}/ghcontrol/templates/create_scripts_from_connections.py
-/usr/bin/python3 /home/${USER}/ghcontrol/templates/get_status.template.py
+/home/${USER}/ghcontrol/run_at_setup/clean_scripts_directory_desktop_entries.sh
+/usr/bin/python3 /home/${USER}/ghcontrol/run_at_setup/create_scripts_from_connections.py
+/usr/bin/python3 /home/${USER}/ghcontrol/run_at_setup/get_status.template.py
 sed "s/USERNAME/${USER}/g" /home/${USER}/ghcontrol/templates/log_event.sh.template > /home/${USER}/ghcontrol/scripts/log_event.sh
-/home/${USER}/ghcontrol/templates/make_scripts_executable.sh
+/home/${USER}/ghcontrol/run_at_setup/make_scripts_executable.sh
