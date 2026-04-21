b419ced9b797d403d0f11c96e133e0eb64d24904
David Smits <david.j.smits@gmail.com>
2026-04-21 12:00:49 -0500

    Use shell files so can read the process easier

---
+++ on_reset/control_temp.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py
---
+++ on_reset/temperature_read.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+/usr/bin/python3 /home/$USER/ghcontrol/on_reset/temperature_read.py
