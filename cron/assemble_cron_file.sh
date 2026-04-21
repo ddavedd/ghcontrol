4b0f7768b766bfedafb4d4431a0162899452ae63
David Smits <david.j.smits@gmail.com>
2026-04-21 18:21:52 -0500

    specify date path more precisely

--- cron/assemble_cron_file.sh
+++ cron/assemble_cron_file.sh
@@ -1,6 +1,6 @@
 cd /home/$USER/ghcontrol/cron
 cat cron_start.cron.template cron_reboot.cron.template cron_status.cron.template cron_graphs_and_report.cron.template cron_haf_schedule.cron.template > assembled.cron
-CRON_LOG='/home/USERNAME/ghcontrol/logs/$(date +"%F").cron.log'
+CRON_LOG='/home/USERNAME/ghcontrol/logs/$(/usr/bin/date +"%F").cron.log'
 sed "s#CRON_LOG#$CRON_LOG#g" assembled.cron > cron_log_replaced.cron
 USERNAME="$(id -un)"
 sed "s/USERNAME/$USERNAME/g" cron_log_replaced.cron > gh.cron
