a1155d783f515f4b22b2fd0916c9ff2f1f3ec0b1
David Smits <david.j.smits@gmail.com>
2026-04-21 22:25:04 -0500

    Change back to call at time of reboot

--- on_reset/heater_restart.sh
+++ on_reset/heater_restart.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/bash
+echo "Running Heater Restart"
 /usr/bin/python3 /home/$(/usr/bin/id -un)/ghcontrol/on_reset/heater_restart.py
--- on_reset/temperature_read.sh
+++ on_reset/temperature_read.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/bash
+echo "Running temperature read"
 /usr/bin/python3 /home/$(/usr/bin/id -un)/ghcontrol/on_reset/temperature_read.py
