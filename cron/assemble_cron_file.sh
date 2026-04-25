96f296094bd2ee597ecc72b3bff519187bebee6b
David Smits <david.j.smits@gmail.com>
2026-04-25 13:35:24 -0500

    Water schedule through cron

--- cron/assemble_cron_file.sh
+++ cron/assemble_cron_file.sh
@@ -1,5 +1,5 @@
 cd /home/$USER/ghcontrol/cron
-cat cron_start.cron.template cron_reboot.cron.template cron_status.cron.template cron_graphs_and_report.cron.template cron_haf_schedule.cron.template > assembled.cron
+cat cron_start.cron.template cron_reboot.cron.template cron_status.cron.template cron_graphs_and_report.cron.template cron_water.cron.template cron_haf_schedule.cron.template > assembled.cron
 CRON_LOG='/home/USERNAME/ghcontrol/logs/$(/home/USERNAME/ghcontrol/cron/date_string.sh).cron.log'
 sed "s#CRON_LOG#$CRON_LOG#g" assembled.cron > cron_log_replaced.cron
 USERNAME="$(id -un)"
