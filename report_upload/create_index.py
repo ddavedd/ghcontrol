be1ac206a84b1bfbf59896643170a0ddb55c59ca
David Smits <david.j.smits@gmail.com>
2026-04-25 17:35:55 -0500

    Using pylint to bring in line with PEP

--- report_upload/create_index.py
+++ report_upload/create_index.py
@@ -1,27 +1,27 @@
+""" Create an index file for our reports and graphs to be uploaded
+"""
 import os
-user = os.getlogin()
+username = os.getlogin()
 print("Report Upload")
-print("User is " + str(user))
-graphs_directory = "/home/%s/ghcontrol/temperature_files/png/" % user
-logs_directory = "/home/%s/ghcontrol/logs/" % user
+print("User is " + str(username))
+graphs_directory = f"/home/{username}/ghcontrol/temperature_files/png/"
+logs_directory = f"/home/{username}/ghcontrol/logs/"
 print("Creating index")
 print("Graphs directory "+graphs_directory)
 print("Logs directory "+logs_directory)
 files_list = []
 for f in os.listdir(graphs_directory):
-   if f.endswith(".png"):
-      files_list.append([f,"./graphs/"+f])
-   
+    if f.endswith(".png"):
+        files_list.append([f,"./graphs/"+f])
 for f in os.listdir(logs_directory):
-   if f.endswith(".log"):
-      files_list.append([f,"./reports/"+f])
+    if f.endswith(".log"):
+        files_list.append([f,"./reports/"+f])
 
 html = "<html>\n<body>\n<ul>\n"
 
 files_list.sort(reverse=True)
 for f in files_list:
-   html += "<li><a href='%s'>%s</a></li>\n" % (f[1],f[0])
+    html += f"<li><a href='{f[1]}'>{f[0]}</a></li>\n"
 html += "</ul>\n</body>\n</html>\n"
-with open("index.html","w") as f:
-   f.write(html)
-
+with open("index.html","w",encoding="utf-8") as f:
+    f.write(html)
