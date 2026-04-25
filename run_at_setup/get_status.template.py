03477062eeef1880277a9110bd0102d1f3c1b53f
David Smits <david.j.smits@gmail.com>
2026-04-25 17:42:14 -0500

    Pylint on get_status.template.py

--- run_at_setup/get_status.template.py
+++ run_at_setup/get_status.template.py
@@ -1,19 +1,15 @@
+""" The template for get status shell file, used to read the status of 3relind & 8relind boards Sequent Microsystems"""
 #! /usr/bin/python3
 import os
 import subprocess
 username = os.getlogin()
 
 for number in [3,8]:
-   output = subprocess.check_output(["/usr/local/bin/%irelind"%number,"-list"], text=True)
-   #output = "2 board(s) detected\nId: 1 0\n"
-   boards = output.split("\n")[1].split(" ")[1:]
-   #text = "sensor_map = []\n"
-   #text += "with open(\"/home/%s/ghcontrol/%s_comp_read.map\" % (username,username)) as mapfile:\n"
-   #text += "\tfor line in mapfile.readlines():\n"
-   #text += "\t\tsensor_map.append(line.strip().split(\",\"))\n"
-   text = ""
-   for b in boards:
-      text += "/home/%s/ghcontrol/scripts/log_event.sh \"/usr/local/bin/%irelind %s read: $(/usr/local/bin/%irelind %s read)\"\n" % (username, number, b, number, b)
-   with open("/home/%s/ghcontrol/scripts/get_status_%i.sh" % (username, number), "w", encoding="utf-8") as f:
-      f.write(text)
-
+    output = subprocess.check_output([f"/usr/local/bin/{number}relind","-list"], text=True)
+    #output = "2 board(s) detected\nId: 1 0\n"
+    boards = output.split("\n")[1].split(" ")[1:]
+    text = ""
+    for b in boards:
+        text += f"/home/{username}/ghcontrol/scripts/log_event.sh \"/usr/local/bin/{number}irelind {b} read: $(/usr/local/bin/{number}relind {b} read)\"\n"
+    with open(f"/home/{username}/ghcontrol/scripts/get_status_{number}.sh", "w", encoding="utf-8") as f:
+        f.write(text)
