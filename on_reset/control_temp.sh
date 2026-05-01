d1b573815ff9546666dab5c5b92ed33eeed7f687
David Smits <david.j.smits@gmail.com>
2026-05-01 13:24:44 -0500

    Trying braces

--- on_reset/control_temp.sh
+++ on_reset/control_temp.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/bash
-echo $(/usr/bin/date +'%F %T') "Running control temperature"
 USER=$(/usr/bin/id -un)
+DATE=$(/usr/bin/date +'%F %T')
+echo $DATE "Running control temperature"
 echo $USER
-/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py &>> /home/$USER/ghcontrol/logs/$(/usr/bin/date +'%F %T').$USER.control.log
+{/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py} >> /home/$USER/ghcontrol/logs/$DATE.$USER.control.log
