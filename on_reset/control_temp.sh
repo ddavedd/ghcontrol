53a58bfdf0889ed1ea6c954df5e6f41e099dd143
David Smits <david.j.smits@gmail.com>
2026-05-01 13:45:39 -0500

    Control temp log filename fixed

--- on_reset/control_temp.sh
+++ on_reset/control_temp.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/bash
 USER=$(/usr/bin/id -un)
-DATE=$(/usr/bin/date +'%F %T')
-LOG_FILE_CONTROL=$(/home/$USER/ghcontrol/logs/$DATE.$USER.control.log)
-echo $DATE $USER "Running control temperature"
-echo "Log file for control is " $LOG_FILE_CONTROL
-{/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py} >> $LOG_FILE_CONTROL
+DATETIME=$(/usr/bin/date +'%F %T')
+DATE=$(/usr/bin/date +'%F')
+LOG_FILE_CONTROL=/home/$USER/ghcontrol/logs/$DATE.$USER.control.log
+echo $DATETIME $USER "Running control temperature"
+echo $LOG_FILE_CONTROL "Log file for control is " $LOG_FILE_CONTROL
+/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py >> $LOG_FILE_CONTROL
