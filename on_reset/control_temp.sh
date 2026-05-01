110812197f5d122b2a7d6f86abaca738913cc754
David Smits <david.j.smits@gmail.com>
2026-05-01 13:33:57 -0500

    Add echo logfile

--- on_reset/control_temp.sh
+++ on_reset/control_temp.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/bash
 USER=$(/usr/bin/id -un)
 DATE=$(/usr/bin/date +'%F %T')
-echo $DATE "Running control temperature"
-echo $USER
-{/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py} >> /home/$USER/ghcontrol/logs/$DATE.$USER.control.log
+LOG_FILE_CONTROL=$(/home/$USER/ghcontrol/logs/$DATE.$USER.control.log)
+echo $DATE $USER "Running control temperature"
+echo "Log file for control is " %LOG_FILE_CONTROL
+{/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py} >> $LOG_FILE_CONTROL
