1e15c9ea718b8389598138173ffa1163ab8def09
David Smits <david.j.smits@gmail.com>
2026-05-01 13:36:53 -0500

    Typo fix

--- on_reset/control_temp.sh
+++ on_reset/control_temp.sh
@@ -3,5 +3,5 @@ USER=$(/usr/bin/id -un)
 DATE=$(/usr/bin/date +'%F %T')
 LOG_FILE_CONTROL=$(/home/$USER/ghcontrol/logs/$DATE.$USER.control.log)
 echo $DATE $USER "Running control temperature"
-echo "Log file for control is " %LOG_FILE_CONTROL
+echo "Log file for control is " $LOG_FILE_CONTROL
 {/usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py} >> $LOG_FILE_CONTROL
