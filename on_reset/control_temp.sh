2a76728e7e5207ea0585ea6f979866e74346923d
David Smits <david.j.smits@gmail.com>
2026-05-01 13:14:32 -0500

    Log

--- on_reset/control_temp.sh
+++ on_reset/control_temp.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
 echo $(/usr/bin/date +'%F %T') "Running control temperature"
 USER=$(/usr/bin/id -un)
+echo $USER
 /usr/bin/python3 /home/$USER/ghcontrol/on_reset/control_temp.py &>> /home/$USER/ghcontrol/logs/$(/usr/bin/date +'%F %T').$USER.control.log
