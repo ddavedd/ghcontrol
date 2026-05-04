2734daaa4ea9f1f8981e8a5f3870576c9b17c5b6
David Smits <david.j.smits@gmail.com>
2026-05-03 19:15:30 -0500

    Write instead of append output

--- status_remote.sh
+++ status_remote.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 USER=$(id -un)
-ssh halfsouth@halfsouth.lan ./ghcontrol/status.sh &>> /home/$USER/ghcontrol/status.log
-cat /home/$USER/ghcontrol/status.log | grep Status: >> /home/$USER/ghcontrol/status.out
+ssh halfsouth@halfsouth.lan ./ghcontrol/status.sh &> /home/$USER/ghcontrol/status.log
+cat /home/$USER/ghcontrol/status.log | grep Status: > /home/$USER/ghcontrol/status.out
 /home/$USER/ghcontrol/scripts/log_event.sh "$(cat /home/$USER/ghcontrol/status.out)"
 #rm /home/$USER/ghcontrol/status.log
 #rm /home/$USER/ghcontrol/status.out
