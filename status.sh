63692eade6bbfcfdd151432f0af115178901cd53
David Smits <david.j.smits@gmail.com>
2026-05-03 19:12:59 -0500

    define user in script

--- status.sh
+++ status.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/bash
+$USER=$(id -un)
 /usr/bin/python3 /home/$USER/ghcontrol/read_status_on.py > /home/$USER/ghcontrol/current_status.txt
 /home/$USER/ghcontrol/scripts/log_event.sh "$(cat /home/$USER/ghcontrol/current_status.txt)"
--- status_remote.sh
+++ status_remote.sh
@@ -1,3 +1,5 @@
+#! /usr/bin/bash
+USER=$(id -un)
 ssh halfsouth@halfsouth.lan ./ghcontrol/status.sh &>> /home/$USER/ghcontrol/status.log
 cat /home/$USER/ghcontrol/status.log | grep Status: >> /home/$USER/ghcontrol/status.out
 /home/$USER/ghcontrol/scripts/log_event.sh "$(cat /home/$USER/ghcontrol/status.out)"
