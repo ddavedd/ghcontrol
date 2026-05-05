1a598d0075103668cdf2994a474421a1e5018d69
David Smits <david.j.smits@gmail.com>
2026-05-05 13:28:59 -0500

    Restart actual file

--- on_reset/restart_temperature_read.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/bash
-TEMPERATURE_FILE="temperature_read.sh"
-TIME=$(date +%H:%M:%S)
-DATE=$(date +%Y-%m-%d)
-USER=$(id -un)
-LOGFILE=/home/$USER/ghcontrol/logs/$DATE.restart.log
-if pgrep -f $TEMPERATURE_FILE; then echo "$TIME $TEMPERATURE_FILE still running"; else echo "$TIME Restarting $TEMPERATURE_FILE"; /usr/bin/bash /home/$USER/ghcontrol/on_reset/$TEMPERATURE_FILE; fi >> $LOGFILE
