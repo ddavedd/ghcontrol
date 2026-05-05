b0d9059768d5a8caef11e1bbd7b4f3cb1c86bc27
David Smits <david.j.smits@gmail.com>
2026-05-05 13:13:59 -0500

    Fixed filename for pgrep

--- on_reset/restart_control_temp.sh
+++ on_reset/restart_control_temp.sh
@@ -4,4 +4,4 @@ TIME=$(date +%H:%M:%S)
 DATE=$(date +%Y-%m-%d)
 USER=$(id -un)
 LOGFILE=/home/$USER/ghcontrol/logs/$DATE.restart.log
-if pgrep -f buttons_setup.py; then echo "$TIME $CONTROL_FILE still running"; else echo "$TIME Restarting $CONTROL_FILE"; fi >> $LOGFILE
+if pgrep -f $CONTROL_FILE; then echo "$TIME $CONTROL_FILE still running"; else echo "$TIME Restarting $CONTROL_FILE"; fi >> $LOGFILE
--- on_reset/restart_temperature_read.sh
+++ on_reset/restart_temperature_read.sh
@@ -4,4 +4,4 @@ TIME=$(date +%H:%M:%S)
 DATE=$(date +%Y-%m-%d)
 USER=$(id -un)
 LOGFILE=/home/$USER/ghcontrol/logs/$DATE.restart.log
-if pgrep -f buttons_setup.py; then echo "$TEMPERATURE_FILE still running"; else echo "$TIME Restarting $TEMPERATURE_FILE"; fi >> $LOGFILE
+if pgrep -f $TEMPERATURE_FILE; then echo "$TIME $TEMPERATURE_FILE still running"; else echo "$TIME Restarting $TEMPERATURE_FILE"; fi >> $LOGFILE
